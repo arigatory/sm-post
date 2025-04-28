f3bf35e0c2f02db4d9fe8d6dd088db4c43726f4a
Ivan Panchenko <arigatory@gmail.com>
2025-04-28 14:39:27 +0300

    feat: add query dispatcher

--- CQRS-ES/CQRS.Core/Events/BaseEvent.cs
+++ CQRS-ES/CQRS.Core/Events/BaseEvent.cs
@@ -6,7 +6,7 @@ public abstract class BaseEvent : Message
 {
     protected BaseEvent(string type)
     {
-        this.Type = type;
+        Type = type;
     }
     public int Version { get; set; }
     public string Type { get; set; }
